afd0190799a1cebf8b6e81afc355f6f5cd9f2f66
youssef_74 <youssefessam1293@gmail.com>
2025-02-09 20:35:46 +0200

    Var

--- Day01LinQ/Program.cs
+++ Day01LinQ/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Implicitly typed local variable
+            #region var
+            //string Data = "Ahmed"; 
+            var Data = "Ahmed";
+            // Implicit Type 
+            // Complier Will Detect The Datatype Of Variable at Compilation Time 
+            // Based On its Intial Value 
+            //var X = null; 
+            // Invalid 
+            // Var Can't be Intialized With Null 
+            Data = null;
+            //Data = 5; 
+            // Invalid 
+            // After Intialization, You Can't Change Variable Datatype 
+            //var X; // Invalid 
+            // Var Must be Intialize 
+            Data "May";
+            #endregion
+            #endregion
         }
     }
 }
