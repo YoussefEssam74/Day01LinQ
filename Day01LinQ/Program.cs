0d787b95f1c8bc5c11a13a61a9af9855a4b7473a
youssef_74 <youssefessam1293@gmail.com>
2025-02-09 22:19:26 +0200

    Anonymous Type

--- Day01LinQ/Program.cs
+++ Day01LinQ/Program.cs
@@ -7,7 +7,7 @@
             #region Implicitly typed local variable
             #region var
             //string Data = "Ahmed"; 
-            var Data = "Ahmed";
+            //var Data = "Ahmed";
             // Implicit Type 
             // Complier Will Detect The Datatype Of Variable at Compilation Time 
             // Based On its Intial Value 
@@ -40,6 +40,35 @@
             // int x = 12345;
             // int y =x.Reverse();
             // Console.WriteLine(y); 
+            #endregion
+            #region Anonymous Type
+            ////Employee employee = new Employee() { Id = 10, Name = "Mona", Salary = 1000}; 
+            ////object Emp01 = new { Id = 20, Name = "Ahmed", Salary = 5000 }; 
+            ////Console.WriteLine(Emp01. Salary); 
+            //// Invalid 
+            //int X = 5;
+            //Console.WriteLine(X.GetType().Name);
+            //// Int32 
+            //var Emp03 = new { Id = 10, Name = "Mona", Salary = 1000 };
+            //Console.WriteLine(Emp03.GetType().Name);// Anonymous Type0'3 
+            //// Object That Will be Created From Anonymous Type Is An Immutable Object [Can not be changed]
+            //Console.WriteLine(Emp03.GetType().Name); // Anonymous Type0'3 
+            //Console.WriteLine(Emp03.Salary); // 1000 
+            //                                 //Emp03.Salary = 2000; // Invalid 
+            //var Emp04 = new { Id = Emp03.Id, Name = Emp03.Name, Salary = 2000 }; // Till C# 9 
+            //var Emp05 = Emp03 with { Salary = 2000 };//c# 10 .net 6
+            //Console.WriteLine($"Emp 03 = {Emp03.GetType().Name}"); ; // Anonymous Type0'3 
+            //Console.WriteLine($"Emp 04 = {Emp04.GetType().Name}"); ; // Anonymous Type0'3 
+            //Console.WriteLine($"Emp 05 = {Emp05.GetType().Name}"); ; // Anonymous Type0'3 
+
+            //// The Same Anonymous Type As Long As : I 
+            //// 1. Same Properties Name [Case Sensitive] 
+            //// 2. Same Properties Order 
+            //var Emp06 = new { Name = "Omar", Salary = 5000, Id = 70 };
+            //Console.WriteLine($"Emp 06 = {Emp06.GetType().Name}"); // _Anonymous Type1'3 
+            //var Emp07 = new { Id = 70, Name = "Salma" };
+            //Console.WriteLine($"Emp 07 = {Emp07.GetType().Name}");// Anonymous Type2 2
+
             #endregion
             #endregion
         }
